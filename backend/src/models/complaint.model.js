a21c9dbe15ff6708ec128fccdc193a1f4e627428
Pragyan Garg <gargpragyan2005@gmail.com>
2025-10-01 12:43:10 +0530

    Complaint Model Setup

--- backend/src/models/complaint.model.js
+++ backend/src/models/complaint.model.js
@@ -1,8 +1,8 @@
 
 import mongoose from "mongoose";
-import { COMPLAINT_STATUS,COMPLAINT_STATUS_ENUM } from "../Enums/ComplaintStatus";
-import { COMPLAINT_TYPE,COMPLAINT_TYPE_ENUM } from "../Enums/ComplaintType";
-import { COMPLAINT_URGENCY,COMPLAINT_URGENCY_ENUM } from "../Enums/ComplaintUrgency";
+import { COMPLAINT_STATUS,COMPLAINT_STATUS_ENUM } from "../enum/ComplaintStatus";
+import { COMPLAINT_TYPE,COMPLAINT_TYPE_ENUM } from "../enum/ComplaintType";
+import { COMPLAINT_URGENCY,COMPLAINT_URGENCY_ENUM } from "../enum/ComplaintUrgency";
 
 const complaintSchema = new mongoose.Schema({
     title: {
