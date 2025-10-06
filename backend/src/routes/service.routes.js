75744ffd4c3c9fc8b1e9d1a90ada40df7b1114b9
Pragyan Garg <gargpragyan2005@gmail.com>
2025-10-06 22:20:58 +0530

    Upvote Functionality Added in Complaints

--- backend/src/routes/service.routes.js
+++ backend/src/routes/service.routes.js
@@ -1,7 +1,7 @@
 import {verifyJWT} from '../middlewares/auth.middleware.js';
 import { Router } from 'express';
 import { getCommonComplaintDashboard } from '../services/user.service.js';
-
+import { ToggleUpvote,getUpvote } from '../controllers/complaint.controller.js';
 
 const router=Router();
 
@@ -9,5 +9,7 @@ router.route('/').get(
     verifyJWT,
     getCommonComplaintDashboard
 )
+router.put("/complaints/:complaintId/toggleUpvote", verifyJWT, ToggleUpvote);
+router.get("/complaints/:complaintId/upvote", verifyJWT, getUpvote);
 
 export default router;
