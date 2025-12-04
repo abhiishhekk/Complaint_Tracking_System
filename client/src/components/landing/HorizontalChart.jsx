bca04b518cc811981ef7f86ae683c42629bc23b3
Abhishek Kumar <abhishekkumar736993@gmail.com>
2025-12-04 18:38:31 +0530

    bar highlighting fixed

--- client/src/components/landing/HorizontalChart.jsx
+++ client/src/components/landing/HorizontalChart.jsx
@@ -9,39 +9,37 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
-
-export default function HorizontalChart({data}) {
+export default function HorizontalChart({ data }) {
   console.log(data);
   return (
     <Box
-        sx={{
-            minHeight: 20,
-            height:"20rem",
-            width:{
-                xs:"90%",
-                sm:"25rem",
-                lg:"30rem"
-            },
-            display:"flex",
-            justifyContent:"center",
-            alignItems:"center",
-        }}
+      sx={{
+        minHeight: 20,
+        height: '20rem',
+        width: {
+          xs: '90%',
+          sm: '25rem',
+          lg: '30rem',
+        },
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        '& .recharts-wrapper *:focus': {
+          outline: 'none !important',
+        },
+      }}
     >
-        <ResponsiveContainer
-        height="100%"
-        width="100%"
-      >
+      <ResponsiveContainer height="100%" width="100%">
         <BarChart
-        
           data={data}
           layout="vertical"
-            margin={{ top: 10, right: 10, bottom: 10, left: 30 }}
+          margin={{ top: 10, right: 10, bottom: 10, left: 30 }}
         >
           {/* <CartesianGrid  /> */}
           <XAxis type="number" />
           <YAxis dataKey="role" type="category" />
           {/* <Tooltip /> */}
-          <Bar dataKey="count" fill="#3f51b5" barSize={30} radius={5}/>
+          <Bar dataKey="count" fill="#3f51b5" barSize={30} radius={5} />
         </BarChart>
       </ResponsiveContainer>
     </Box>
